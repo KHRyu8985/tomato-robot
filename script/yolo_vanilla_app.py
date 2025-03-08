815a963d86a971eb209471b234ada52e9563cdf4
Sumin Cho <sumincho0830@gmail.com>
2025-03-08 17:11:54 +0900

    YOLOv10 real-time detection demo app updated

---
+++ script/yolo_vanilla_app.py
@@ -0,0 +1,87 @@
+import sys
+import os
+import av 
+import cv2
+import tempfile
+import supervision as sv
+import gradio as gr
+from gradio_webrtc import WebRTC
+from huggingface_hub import hf_hub_download
+
+# Get the absolute path of the script
+script_path = os.path.abspath(__file__)
+
+# Get the directory containing the script
+script_dir = os.path.dirname(script_path)
+
+# Get the project root directory (one level up from script directory)
+project_root = os.path.abspath(os.path.join(script_dir, '..'))
+
+# Add the project root to the Python path
+sys.path.append(project_root)
+
+# Now you can import modules from src
+print(f"Added {project_root} to Python path")
+
+from src.yolo_model.yolov10 import YOLOv10
+
+
+
+
+
+model_file = hf_hub_download(
+    repo_id="onnx-community/yolov10n", filename="onnx/model.onnx"
+)
+
+model = YOLOv10(model_file)
+
+
+rtc_configuration = None
+
+
+# def pass_frame(frame):
+#     # The Femto-bolt camera returns frames in a different format, thus requires additional processing
+#     # The frame argument is often a av.VideoFrame object from PyAV
+#     return frame
+
+
+
+def pass_frame(frame: av.VideoFrame) -> av.VideoFrame:
+     # Convert the incoming frame to a NumPy array (BGR)
+    image_bgr = frame.to_ndarray(format="bgr24")
+
+    # Conver the processed NumPy array back to an av.VideoFrame object
+    return av.VideoFrame.from_ndarray(image_bgr, format="bgr24")
+
+
+def detection(image, conf_threshold=0.3):
+    image = cv2.resize(image, (model.input_width, model.input_height))
+    new_image = model.detect_objects(image, conf_threshold)
+
+    return new_image
+
+   
+
+css = """.my-group {max-width: 600px !important; max-height: 600px !important;}
+         .my-column {display: flex !important; justify-content: center !important; align-items:center !important;}"""
+with gr.Blocks(css=css) as demo:
+    gr.HTML(
+        """
+        <h1 style="text-align: center"> YOLOv10 - Demo </h1>
+        """
+    )
+
+    with gr.Column(elem_classes=["my-column"]):
+        with gr.Group(elem_classes=["my-group"]):
+           
+            image = WebRTC(label="Stream", rtc_configuration=rtc_configuration) # WebRTC typically requires a valid RTC configuration dictionary
+            image.stream(
+                fn=detection, inputs=image, outputs=image
+            )
+
+
+
+# In python, every module has a built-in variable called __name__. When you run a Python file directly, python sets __name__ to "__main__"
+# This ensures demo.launch() is only called when you intent run this file directly
+if __name__ == "__main__":
+    demo.launch(share=True)
---
+++ src/yolo_model/utils.py
@@ -0,0 +1,223 @@
+import numpy as np
+import cv2
+
+# 이 부분을 Florence2의 Open Vocabulary Task로 치환할 수 있음
+class_names=[
+    "person",
+    "bicycle",
+    "car",
+    "motorcycle",
+    "airplane",
+    "bus",
+    "train",
+    "truck",
+    "boat",
+    "traffic light",
+    "fire hydrant",
+    "stop sign",
+    "parking meter",
+    "bench",
+    "bird",
+    "cat",
+    "dog",
+    "horse",
+    "sheep",
+    "cow",
+    "elephant",
+    "bear",
+    "zebra",
+    "giraffe",
+    "backpack",
+    "umbrella",
+    "handbag",
+    "tie",
+    "suitcase",
+    "frisbee",
+    "skis",
+    "snowboard",
+    "sports ball",
+    "kite",
+    "baseball bat",
+    "baseball glove",
+    "skateboard",
+    "surfboard",
+    "tennis racket",
+    "bottle",
+    "wine glass",
+    "cup",
+    "fork",
+    "knife",
+    "spoon",
+    "bowl",
+    "banana",
+    "apple",
+    "sandwich",
+    "orange",
+    "broccoli",
+    "carrot",
+    "hot dog",
+    "pizza",
+    "donut",
+    "cake",
+    "chair",
+    "couch",
+    "potted plant",
+    "bed",
+    "dining table",
+    "toilet",
+    "tv",
+    "laptop",
+    "mouse",
+    "remote",
+    "keyboard",
+    "cell phone",
+    "microwave",
+    "oven",
+    "toaster",
+    "sink",
+    "refrigerator",
+    "book",
+    "clock",
+    "vase",
+    "scissors",
+    "teddy bear",
+    "hair drier",
+    "toothbrush",
+]
+
+# Create a list of colors for each class where each color is a tuple of 3 integer values
+rng = np.random.default_rng(3) # random number generator
+colors = rng.uniform(0, 255, size=(len(class_names),3)) # genrates random colors from a uniform distribution between 0 and 255
+
+def nms(boxes, scores, iou_threshold):
+    # Sort by score
+    sorted_indices = np.argsort(scores)[::-1]
+    # argsort() returns an arary of indices that would sort the scores array in an ascending order
+    # [::-1] reverses the order of the sorted indices
+
+    keep_boxes = []
+    while sorted_indices.size > 0:
+        # Pick the last box
+        box_id = sorted_indices[0]
+        keep_boxes.append(box_id)
+
+        # Comptue IoU of the picked box with the rest
+        ious = compute_iou(boxes[box_id, :], boxes[sorted_indices[1:], :])
+        
+
+def multiclass_nms(boxes, scores, class_ids, iou_threshold):
+    unique_class_ids = np.unique(class_ids)
+
+    keep_boxes = []
+    for class_id in unique_class_ids:
+        class_indices = np.where(class_ids == class_id)[0]
+        class_boxes = boxes[class_indices, :]
+        class_scores = scores[class_indices]
+
+        class_keep_boxes = nms(class_boxes, class_scores, iou_threshold)
+        keep_boxes.extend(class_indices[class_keep_boxes])
+
+    return keep_boxes
+
+
+
+def compute_iou(box, boxes):
+    # Compute xmin, ymin, xmax, ymax for both boxes
+    xmin = np.maximum(box[0], boxes[:, 0])
+    ymin = np.maximum(box[1], boxes[:, 1])
+    xmax = np.minimum(box[2], boxes[:, 2])
+    ymax = np.minimum(box[3], boxes[:, 3])
+
+    # Compute intersection area
+    intersection_area = np.maximum(0, xmax - xmin) * np.maximum(0, ymax - ymin)
+
+    # Compute union area
+    box_area = (box[2] - box[0]) * (box[3] - box[1])
+    boxes_area = (boxes[:, 2] - boxes[:, 0]) * (boxes[:, 3] - boxes[:, 1])
+    union_area = box_area + boxes_area - intersection_area
+
+    # Compute IoU
+    iou = intersection_area / union_area
+
+    return iou
+
+
+def draw_detections(image, boxes, scores, class_ids, mask_alpha=0.3):
+    det_img = image.copy()
+
+    img_height, img_width = image.shape[:2]
+    font_size = min([img_height, img_width]) * 0.0006
+    text_thickness = int(min([img_height, img_width]) * 0.001)
+
+    #det_img = draw_masks(det_img, boxes, class_ids, mask_alpha)
+
+    # Draw bounding boxes and labels of detections
+    for class_id, box, score in zip(class_ids, boxes, scores):
+        color = colors[class_id]
+
+        draw_box(det_img, box, color)
+
+        label = class_names[class_id]
+        caption = f"{label} {int(score * 100)}%"
+        draw_text(det_img, caption, box, color, font_size, text_thickness)
+
+    return det_img
+
+
+def draw_box(
+        image: np.ndarray,
+        box: np.ndarray,
+        color: tuple[int, int, int] = (0,0,255),
+        thickness: int =2
+) -> np.ndarray:
+    x1, y1, x2, y2 = box.astype(int)
+    return cv2.rectangle(image, (x1, y1), (x2, y2), color, thickness)
+
+
+def draw_text(
+        image: np.ndarray,
+        text: str,
+        box: np.ndarray,
+        color: tuple[int, int, int] = (0,0,255),
+        font_size: float=0.001,
+        text_thickness: int = 2
+) -> np.ndarray:
+    x1, y1, x2, y2 = box.astype(int)
+    (tw, th), _ = cv2.getTextSize(
+        text=text,
+        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+        fontScale=font_size,
+        thickness=text_thickness
+    )
+
+    th = int(th * 1.2)
+
+    cv2.rectangle(image, (x1, y1), (x1+tw, y1 - th), color, -1)
+
+    return cv2.putText(
+        image,
+        text,
+        (x1, y1),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        font_size,
+        (255, 255, 255),
+        text_thickness,
+        cv2.LINE_AA
+    )
+
+
+def draw_masks(
+        image: np.ndarray, boxes: np.ndarray, classes: np.ndarray, mask_alpha: float = 0.3
+) -> np.ndarray:
+    mask_img = image.copy()
+
+    # Draw bounding boxes and labels of detections
+    for box, class_id in zip(boxes, classes):
+        color = colors[class_id]
+
+        x1, y1, x2, y2 = box.astype(int)
+
+        # Draw fill rectangle in mask image
+        cv2.rectangle(mask_img, (x1, y1), (x2, y2), color, -1) # -1 means fill the rectangle
+
+        cv2.addWeighted(mask_img, mask_alpha, image, 1 - mask_alpha, 0)
